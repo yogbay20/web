b4caaf16d6afe368bbdf810b53a22bc9d9530c84
yogbay20 <yogabayu834@gmail.com>
2024-11-21 14:35:56 +0700

    seccond

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+  const dropdown = document.getElementById('dropdown');
+  dropdown.classList.toggle('show');
+  const button = document.getElementById('menu-btn');
+  button.classList.toggle('hide');
+}
+
+// Close the menu if clicked outside
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-btn')) {
+    const dropdown = document.getElementById('dropdown');
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+  if (!event.target.matches('.menu-btn')) {
+    const button = document.getElementById('menu-btn');
+    if (button.classList.contains('hide')) {
+      button.classList.remove('hide');
+    }
+  }
+}
